aec5a598d0ba447aa0db01ca2125c5b3b1f4e6ba
dxkolesov <dxkolesov@gmail.com>
2025-04-30 19:14:48 +0300

    feat(snacks): use default dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -36,73 +36,9 @@ return {
         },
       }
 
-      -- dashboard
-      -- header
-      local cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
-      if #cwd > 60 then
-        cwd = "..." .. cwd:sub(-57)
-      end
+      -- dashboard header
       opts.dashboard.preset.header = {
-        cwd,
-      }
-
-      -- sections
-      opts.dashboard.sections = {
-        { section = "header", padding = 1 },
-        { section = "keys", gap = 1, padding = 2 },
-        function()
-          local in_git = Snacks.git.get_root() ~= nil
-          local cmds = {
-            {
-              title = "Notifications",
-              cmd = "gh notify -s -a -n5",
-              action = function()
-                vim.ui.open("https://github.com/notifications")
-              end,
-              key = "N",
-              icon = " ",
-              height = 5,
-              enabled = true,
-            },
-            {
-              title = "Open Issues",
-              cmd = "gh issue list -L 3",
-              key = "i",
-              action = function()
-                vim.fn.jobstart("gh issue list --web", { detach = true })
-              end,
-              icon = " ",
-              height = 7,
-            },
-            {
-              icon = " ",
-              title = "Open PRs",
-              cmd = "gh pr list -L 3",
-              key = "p",
-              action = function()
-                vim.fn.jobstart("gh pr list --web", { detach = true })
-              end,
-              height = 7,
-            },
-            {
-              icon = " ",
-              title = "Git Status",
-              cmd = "git --no-pager diff --stat -B -M -C",
-              height = 10,
-            },
-          }
-          return vim.tbl_map(function(cmd)
-            return vim.tbl_extend("force", {
-              pane = 2,
-              section = "terminal",
-              enabled = in_git,
-              padding = 1,
-              ttl = 5 * 60,
-              indent = 3,
-            }, cmd)
-          end, cmds)
-        end,
-        { section = "startup" },
+        vim.fn.fnamemodify(vim.fn.getcwd(), ":~"),
       }
 
       -- highlights
