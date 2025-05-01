5eeb402caa2e7d8ec92ea3d18f62028d64c1231a
dxkolesov <dxkolesov@gmail.com>
2025-05-01 16:46:40 +0300

    feat(fugutive): close fugitive with <q>

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,19 @@
 return {
   {
     "tpope/vim-fugitive",
+    -- close fugutive with <q>
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "fugitive",
+        callback = function(event)
+          vim.keymap.set("n", "q", "<cmd>close<CR>", {
+            buffer = event.buf,
+            silent = true,
+            desc = "Close fugitive buffer",
+          })
+        end,
+      })
+    end,
     keys = {
       {
         "<leader>gi",
