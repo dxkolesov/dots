56c51a0204da2aaf53fa3aa0be455d4b61ce1231
dxkolesov <dxkolesov@gmail.com>
2025-07-29 20:36:01 +0300

    feat(harpoon): add harpoon instead of bufferline

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
 
   -- colorschemes
   { "folke/tokyonight.nvim", enabled = false },
