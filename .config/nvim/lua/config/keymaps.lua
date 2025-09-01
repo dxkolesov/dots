55ae213f13eeea44e5c947a49da512fb60a1506c
dxkolesov <dxkolesov@gmail.com>
2025-09-01 19:09:16 +0300

    feat(nvim): add resize keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,7 +13,12 @@ set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center", silent = true })
 set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center", silent = true })
 set("n", "n", "nzzzv", { desc = "Next search result centered", silent = true })
 set("n", "N", "Nzzzv", { desc = "Previous search result centered", silent = true })
-set("n", "G", "Gzz", { desc = "Go to bottom and center", silent = true })
+
+-- resize window
+set("n", "<C-left>", "<C-w><")
+set("n", "<C-right>", "<C-w>>")
+set("n", "<C-up>", "<C-w>+")
+set("n", "<C-down>", "<C-w>-")
 
 -- ai agent path
 local function get_ai_path()
@@ -24,7 +29,6 @@ end
 
 local function copy_ai_path(path)
   vim.fn.setreg("+", path .. " ")
-  vim.notify("Copied AI agent path: " .. path)
 end
 
 set("n", "<leader>a", "", { desc = "+ai", silent = true })
@@ -33,7 +37,7 @@ set("v", "<leader>a", "", { desc = "+ai", silent = true })
 set("n", "<leader>as", function()
   local ai_path = get_ai_path()
   copy_ai_path(ai_path)
-end, { desc = "Copy path for AI terminal agents" })
+end, { desc = "copy path" })
 
 set("v", "<leader>as", function()
   local ai_path = get_ai_path()
@@ -48,4 +52,4 @@ set("v", "<leader>as", function()
   vim.defer_fn(function()
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
   end, 10)
-end, { desc = "Copy path with line numbers for AI terminal agents" })
+end, { desc = "copy path" })
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -72,7 +72,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
